ae6382b28f4255e1f809c94e30783cd145c7a880
tediousgraffiti <tediousgraffiti@gmail.com>
2023-02-14 15:33:55 -0600

    learning in progress

--- cider.py
+++ cider.py
@@ -4,31 +4,47 @@ import argparse
 import re
 import sys
 
+MAX_IPV4 = 2**32 - 1
 
-def cidr_to_subnet(address, cidr):
-    complement = 32 - cidr
-    mask_len = complement % 8
-    mask = 256 - mask_len
-    octet = cidr // 8 + 1
-    group_size = 2**complement
 
-    if octet == 1:
-        subnet_mask = f"{mask}.0.0.0"
-    elif octet == 2:
-        subnet_mask = f"255.{mask}.0.0"
-    elif octet == 3:
-        subnet_mask = f"255.255.{mask}.0"
-    else:
-        subnet_mask = f"255.255.255.{mask}"
+def cidr_to_subnet(address: str, prefix: int):
+    subnet_mask_dec = MAX_IPV4 ^ (MAX_IPV4 >> prefix)
+    subnet_mask_bin = bin(subnet_mask_dec)[2:]
+    subnet_mask_str = ".".join(
+        map(
+            lambda x: str(int(x, base=2)),
+            (subnet_mask_bin[i * 8 : (i + 1) * 8] for i in range(4)),
+        )
+    )
 
+    host_count = 2 ** (32 - prefix)
+
+    # address_dec = address.split(".")
+    # network_id = address ^ subnet_mask_bin
+
+    # complement = 32 - prefix
+    # mask_len = complement % 8
+    # mask = 256 - mask_len
+    # octet = cidr // 8 + 1
+    # group_size = 2**complement
+
+    # if octet == 1:
+    #     subnet_mask = f"{mask}.0.0.0"
+    # elif octet == 2:
+    #     subnet_mask = f"255.{mask}.0.0"
+    # elif octet == 3:
+    #     subnet_mask = f"255.255.{mask}.0"
+    # else:
+    #     subnet_mask = f"255.255.255.{mask}"
+    group_size = 42
     return {
-        "subnet_mask": subnet_mask,
+        "subnet_mask": subnet_mask_str,
         "network_id": "blah",
         "next_network": "blah",
         "broadcast_ip": "blah",
         "first_host": "blah",
         "last_host": "blah",
-        "addressable_hosts": group_size - 2,
+        "addressable_hosts": host_count - 2,
     }
 
 
@@ -40,6 +56,6 @@ if __name__ == "__main__":
         r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\/(?:[1-9]|[12][0-9]|3[0-2])$"  # credit: https://www.oreilly.com/library/view/regular-expressions-cookbook/9780596802837/ch07s16.html
     )
     assert pat.match(address)
-    ip, cidr = address.split("/")
-    results = cidr_to_subnet(ip, cidr)
+    ip, cidr_prefix = address.split("/")
+    results = cidr_to_subnet(ip, int(cidr_prefix))
     print(results)
--- readme.md
+++ readme.md
@@ -8,6 +8,10 @@ Give it a CIDR and it'll report the network details. Alternatively, give it an i
 
 Future work might include suggesting networks from simple inputs.
 
+Update: lol of course this is [built into the standard library](https://docs.python.org/3/library/ipaddress.html)...Alright, will have to do two passes, one the hard way, one with the provided batteries. Key idea there seems to be to convert everything to big'ol 32 bit integers and do all the operations there before converting back.
+
 ## References
 
 * [Practical Networking - Subnetting Mastery](https://www.youtube.com/watch?v=BWZ-MHIhqjM&list=PLIFyRwBY_4bQUE4IB5c4VPRyDoLgOdExE&index=2&ab_channel=PracticalNetworking)
+* [identify network and broadcast address of a subnet](https://www.countryipblocks.net/blog/identifying-the-network-and-broadcast-address-of-a-subnet/)
+* [ipaddress.py source](https://github.com/python/cpython/blob/4aeae286715a3a9fa624429733582917606000c3/Lib/ipaddress.py)
